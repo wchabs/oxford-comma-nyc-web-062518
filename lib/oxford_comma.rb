013ddc108f4eadb2bcbedcec58a04837eaf829bb
William Chabla <w_chabla@hotmail.com>
2018-06-21 22:48:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  num_of_elements = array.size
+  if num_of_elements == 1
+    array[0]
+  elsif num_of_elements == 2
+    array.join(" and ")
+  elsif num_of_elements >= 3
+    last_element = array.pop
+    phrase = array.join(", ")
+    phrase += ", and #{last_element}"
+  end
 end
